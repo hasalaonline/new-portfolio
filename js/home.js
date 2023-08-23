c051fb5907fd4a668d21f7d0eca40c3374690549
Hasala Abhilasha <hasalaonline@gmail.com>
2023-08-23 11:36:40 +0530

    Upgraded

--- js/home.js
+++ js/home.js
@@ -5,9 +5,9 @@ function isMobileView() {
   if (isMobileView()) {
     window.addEventListener('load', function() {
       var image = document.querySelector('.zoom-image');
-      var scale = 1.7;
-      var duration = 1000;
-      var delay = 500;
+      var scale = 1.5;
+      var duration = 0;
+      var delay = 0;
       
       image.style.transition = 'transform ' + duration + 'ms ease-out ' + delay + 'ms';
       image.style.transform = 'scale(' + scale + ')';
@@ -15,12 +15,23 @@ function isMobileView() {
   
     window.addEventListener('scroll', function() {
       var image = document.querySelector('.zoom-image');
+      var hero_image = document.querySelector('.hero-image')
+      var hero_name = document.querySelector('.container')
+      var hero_container = document.querySelector('.hero-container')
       var scale = 0.8;
-      var duration = 500;
-      var delay = 100;
+      var duration = 1500;
+      var delay = 500;
+
+      var hero_scale = 0.5
       
-      image.style.transition = 'transform ' + duration + 'ms ease-in ' + delay + 'ms';
-      image.style.transform = 'scale(' + scale + ')';
+    //   image.style.transition = 'transform ' + duration + 'ms ease-in ' + delay + 'ms';
+    //   image.style.transform = 'scale(' + scale + ')';
+
+    //   hero_image.style.transition = 'transform ' + duration + 'ms ease-out ' + delay + 'ms';
+    //   hero_image.style.transform = 'scale(' + hero_scale + ')';
+
+    hero_container.style.transition = 'transform ' + duration + 'ms ease-out ' + delay + 'ms';
+    hero_container.style.transform = 'scale(' + hero_scale + ')';
     });
   }
   
